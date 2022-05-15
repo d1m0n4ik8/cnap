cb9e4055866cf4f65d6a9cc454377b39791049ee
d1m0n4ik <svyryd2012@gmail.com>
2022-05-15 15:59:32 +0300

    add page "contacts"

--- js/app.js
+++ js/app.js
@@ -5,24 +5,25 @@ let listItems = document.querySelectorAll('.list-group-item')
 let tabPanels = document.querySelectorAll('.tab-pane')
 
 function scrollToElement() {
-	tabContent.scrollIntoView({ block: "center", behavior: "smooth" })
+   tabContent.scrollIntoView({ block: 'center', behavior: 'smooth' })
 }
 
 function hideTab() {
-	for (let i = 0; i < tabPanels.length; i++) {
-		tabPanels[i].classList.remove('active', 'show')
-	}
+   for (let i = 0; i < tabPanels.length; i++) {
+      tabPanels[i].classList.remove('active', 'show')
+   }
 }
 function showTab() {
-	tabPanels[value].classList.add('active', 'show')
+   tabPanels[value].classList.add('active', 'show')
 }
 
 for (let i = 0; i < links.length; i++) {
-	links[i].addEventListener('click', function () {
-		localStorage.setItem('index', i)
-	})
+   links[i].addEventListener('click', function () {
+      localStorage.setItem('index', i)
+   })
 }
-let value = localStorage.getItem('index');
+let value = +localStorage.getItem('index')
+
 hideTab()
 showTab()
 localStorage.clear()
